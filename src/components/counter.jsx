769df8058c5d528b4ecc7c5bfc7d81b03a714139
Rajni Singh <rajnikantsingh.external@gmail.com>
2020-12-23 22:51:40 +1300

    added button for start stop

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,28 +1,10 @@
 import React, { Component } from "react";
 class Counter extends Component {
 	state = {
-		tags2: [],
-		tags: ["tag1", "tag2", "tag3"],
-		// imageUrl : 'https://picsum.photos/200'
+		buttonText: "Start",
 	};
-
-	styles = {
-		fontSize: 130,
-		fontWeight: "bold",
-	};
-
-	renderTags() {
-		if (this.state.tags.length === 0) return <p>There are no Tags</p>;
-		return (
-			<ul>
-				{this.state.tags.map((tag) => (
-					<li key={tag}>{tag} </li>
-				))}
-			</ul>
-		);
-	}
-
 	render() {
+		const buttonColor = this.state.buttonText === "Stop" ? "danger" : "success";
 		return (
 			<React.Fragment>
 				<h4>Counter #{this.props.counter.id}</h4>
@@ -35,9 +17,15 @@ class Counter extends Component {
 				</button>
 				<button
 					onClick={() => this.props.onDelete(this.props.counter.id)}
-					className="btn btn-danger btn-sm m-2"
+					className="btn btn-warning btn-sm m-2"
 				>
 					Delete
+				</button>
+				<button
+					onClick={() => this.flipStartStop()}
+					className={`btn btn-${buttonColor}`}
+				>
+					{this.state.buttonText}
 				</button>
 			</React.Fragment>
 		);
@@ -52,6 +40,16 @@ class Counter extends Component {
 	formatCount() {
 		const { value: count } = this.props.counter;
 		return count === 0 ? "Zero" : count;
+	}
+
+	flipStartStop() {
+		if (this.state.buttonText == "Start") {
+			const buttonText = "Stop";
+			this.setState({ buttonText });
+		} else {
+			const buttonText = "Start";
+			this.setState({ buttonText });
+		}
 	}
 }
 
